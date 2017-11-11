5eb8bc7cc3e2ad771eebc1fd5b50fd42d6dca408
Yashren Reddi (Laptop) <yreddi@spaceteq.co.za>
2017-11-11 17:29:12 +0200

    compute stats

--- tetrahedral_test/src/main.c
+++ tetrahedral_test/src/main.c
@@ -12,6 +12,10 @@
 #include "CciProtocol.h"
 #include "ekf.h"
 
+float32_t computeMean(float32_t m, float32_t x, float32_t k);
+float32_t computeVariance(float32_t v, float32_t x, float32_t m, float32_t m_k);
+void computeGyroStats(float32_t gyro[3], float32_t accel[3], float32_t mag[3], float32_t gyro_var[3], float32_t accel_var[3], float32_t mag_var[3]);
+
 __IO uint32_t SysTickCounter = 0;
 __IO int32_t DelayCounter = 0;
 void SysTick_Handler(void){
@@ -34,12 +38,17 @@ typedef struct{
 void computeInitMeasurementFrame(float32_t y[6]){
 	int i;
 	double measurements[6] = {0};
+	float32_t gyro_var[3] = {0};
+	float32_t accel_var[3] = {0};
+	float32_t mag_var[3] = {0};
 	imu_data_s imu_data;
 	int8_t temperature;
 
-	for (i = 0; i < 40; i++){
+	for (i = 0; i < 50; i++){
+		read_gyro(I2C1, imu_data.rate, &temperature);
 		read_accel(I2C1, imu_data.acceleration);
 		read_mag(I2C1, imu_data.magnetic);
+		computeGyroStats(imu_data.rate, imu_data.acceleration, imu_data.magnetic, gyro_var, accel_var, mag_var);
 		// store data
 		measurements[0] += (double)imu_data.acceleration[0];
 		measurements[1] += (double)imu_data.acceleration[1];
@@ -51,11 +60,67 @@ void computeInitMeasurementFrame(float32_t y[6]){
 	}
 
 	for (i = 0; i < 6; i++)
-		measurements[i] /= 40.0;
+		measurements[i] /= 50.0;
 
 	for (i = 0; i < 6; i++)
 		y[i] = (float32_t)measurements[i];
 
+}
+
+float32_t computeMean(float32_t m, float32_t x, float32_t k){
+	return m + (x - m)/k;
+}
+
+float32_t computeVariance(float32_t v, float32_t x, float32_t m, float32_t m_k){
+	return v + (x - m)*(x - m_k);
+}
+
+void computeGyroStats(float32_t gyro[3], float32_t accel[3], float32_t mag[3], float32_t gyro_var[3], float32_t accel_var[3], float32_t mag_var[3]){
+	static float32_t m_g[3] = {0};
+	static float32_t v_g[3] = {0};
+	static float32_t m_a[3] = {0};
+	static float32_t v_a[3] = {0};
+	static float32_t m_m[3] = {0};
+	static float32_t v_m[3] = {0};
+
+	static uint32_t count = 0;
+	static bool first_enter = true;
+	int i;
+
+	if (first_enter){
+		for (i = 0; i < 3; i++){
+			v_g[i] = 0.0f;
+			m_g[i] = gyro[i];
+			v_a[i] = 0.0f;
+			m_a[i] = accel[i];
+			v_m[i] = 0.0f;
+			m_m[i] = mag[i];
+		}
+
+		count++;
+		first_enter = false;
+		return;
+	}
+
+	float32_t m_k;
+	for (i = 0; i < 3; i++){
+		m_k = computeMean(m_g[i], gyro[i], (float32_t)count);
+		v_g[i] = computeVariance(v_g[i], gyro[i], m_g[i], m_k);
+		m_g[i] = m_k;
+
+		m_k = computeMean(m_a[i], accel[i], (float32_t)count);
+		v_a[i] = computeVariance(v_a[i], accel[i], m_a[i], m_k);
+		m_a[i] = m_k;
+
+		m_k = computeMean(m_m[i], mag[i], (float32_t)count);
+		v_m[i] = computeVariance(v_m[i], mag[i], m_m[i], m_k);
+		m_m[i] = m_k;
+
+		count++;
+		gyro_var[i] = v_g[i]/((float32_t)count - 1.0f);
+		accel_var[i] = v_a[i]/((float32_t)count - 1.0f);
+		mag_var[i] = v_m[i]/((float32_t)count - 1.0f);
+	}
 }
 
 int main(void) {
